f4a83a9be86413483c9dfa1898e77ac5e93b29ba
jmo.py <jafet.moraugalde15@gmail.com>
2021-08-21 20:16:43 -0600

    Add responsiveness to the calendar component. Add the title of the app and the copyright. Create the year selector.

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,6 +1,6 @@
 import { CalendarEvent, CalendarEventAction, CalendarEventTimesChangedEvent, CalendarView } from 'angular-calendar';
 import { startOfDay, endOfDay, subDays, addDays, endOfMonth, isSameDay, isSameMonth, addHours, parseISO } from 'date-fns';
-import { Component, ChangeDetectionStrategy, ViewChild, TemplateRef, OnInit, ElementRef } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ViewChild, TemplateRef, OnInit, ElementRef, AfterViewInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Subject } from 'rxjs';
 import { CreateAppointmentsComponent } from './create-appointments/create-appointments.component';
@@ -14,12 +14,17 @@ import { AppointmentService } from '../http-services/appointments/appointment.se
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
-export class CalendarComponent implements OnInit {
+export class CalendarComponent implements OnInit, AfterViewInit {
   @ViewChild('modalContent', { static: true }) modalContent: TemplateRef<any> | undefined;
 
   view: CalendarView = CalendarView.Month;
   CalendarView = CalendarView;
   viewDate: Date = new Date();
+
+  years = [2018, 2019, 2020, 2021, 2022, 2023]
+  months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+  slideIndex = 1;
 
   modalData: {
     action: string;
@@ -42,6 +47,10 @@ export class CalendarComponent implements OnInit {
     private appointmentService: AppointmentService
   ) {
     document.getElementById('calendar')?.focus();
+  }
+
+  ngAfterViewInit(): void {
+    this.showSlides(this.slideIndex);
   }
 
   dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
@@ -131,4 +140,19 @@ export class CalendarComponent implements OnInit {
     );
   }
 
+  plusSlides(n: number) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  showSlides(n: number) {
+    console.log('here');
+    let i;
+    let slides = Array.from(document.getElementsByClassName("year") as HTMLCollectionOf<HTMLElement>);
+    if (n > slides.length) { this.slideIndex = 1 }
+    if (n < 1) { this.slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[this.slideIndex - 1].style.display = "block";
+  }
 }
